728b69cc2ca94938917e3057a9f8aa97506c5fe0
juju8899 <146299878+juju8899@users.noreply.github.com>
2023-12-15 20:34:01 +0000

    投稿機能

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -2,10 +2,11 @@ class Public::PostsController < ApplicationController
   def new
     @post = Post.new
   end
-  
+
   def search
     if params[:keyword]
       @books = RakutenWebService::Books::Book.search(title: params[:keyword])
+      # redirect_to new_post_path(title: @books.first.title, author: @books.first.author, summary: @books.first.summary, image_url: @books.first.medium_image_url)
     end
   end
 
