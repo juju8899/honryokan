c7998becdec29d93bbbd510da8b0240e4adcd6a7
juju8899 <146299878+juju8899@users.noreply.github.com>
2023-12-25 11:19:32 +0000

    about修正

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -2,11 +2,17 @@ class Public::CommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    post = Post.find(params[:post_id])
-    comment = current_user.comments.new(comment_params)
-    comment.post_id = post.id
-    comment.save
-    redirect_to post_path(post), notice: "コメントを投稿しました"
+    @post = Post.find(params[:post_id])
+    @comment = current_user.comments.new(comment_params)
+    @comment.post_id = @post.id
+    if @comment.save
+      redirect_to post_path(@post), notice: "コメントを投稿しました"
+    else
+      @tag_list = @post.post_tags.joins(:tag).pluck('tags.name').join(',')
+      @post_tags = @post.tags
+      @user = @post.user
+      render "public/posts/show"
+    end
   end
 
   def destroy
